6f36cb339611ca785c42079699fbf71630a7f2e1
martaalcazarr <martaalcazarredondo@gmail.com>
2023-08-18 19:25:28 -0400

    add form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,39 @@
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div className="container">
+        <form>
+            <div className="field">
+                <label htmlFor="city">City</label>
+                <input 
+                    type="text" 
+                    id="city"
+                    name="city"
+                />
+            </div>
+
+            <div className="field">
+                <label htmlFor="country">Country</label>
+               <select name="country" id="country">
+                <option value="">--Select a country--</option>
+                <option value="CL">Chile</option>
+                <option value="ES">Spain</option>
+                <option value="MX">Mexico</option>
+                <option value="PE">Peru</option>
+                <option value="AR">Argentina</option>
+                <option value="CO">Colombia</option>
+                <option value="US">United States</option>
+                <option value="FR">France</option>
+                <option value="IT">Italy</option>
+               </select>
+            </div>
+
+            <input 
+                type="submit" 
+                value="Check weather"
+            />
+        </form>
+    </div>
   )
 }
 
